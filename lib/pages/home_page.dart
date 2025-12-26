68bd65f8ae4b70c30cba6a1c4b4d939b1ae860af
Dri Bevilaqua <andrielebevilaqua@gmail.com>
2025-12-26 20:16:49 -0300

    mensagem

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -19,13 +19,61 @@ class HomePage extends StatefulWidget {
         centerTitle: true,
         backgroundColor: Colors.green,
         title: Text('AppBar', style: TextStyle(color: Colors.white),),
-        
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.green),
+              child: UserAccountsDrawerHeader(decoration: BoxDecoration(color: Colors.green), 
+              accountName: Text('Andriele'), accountEmail: Text('andriele@example.com'),
+              currentAccountPicture: CircleAvatar(backgroundColor: Colors.greenAccent, child: Text('A'),))
+              ),
+              ListTile(
+                leading: Icon (Icons.home),
+                title: Text('Home'),
+                trailing: Icon(Icons.arrow_forward),
+                onTap: (){
+                  _pageController.jumpToPage(0);
+                  Navigator.pop(context);
+                    setState(() {
+                  indexBottomNavegigationBar = 0;
+            });
+                },
+              ),
+              ListTile(
+                leading: Icon (Icons.person),
+                title: Text('Profile'),
+                trailing: Icon(Icons.arrow_forward),
+               onTap: (){
+                  _pageController.jumpToPage(1);
+                  Navigator.pop(context);
+                    setState(() {
+                 indexBottomNavegigationBar = 1;
+            });
+                },
+              ),
+              ListTile(
+                leading: Icon (Icons.settings),
+                title: Text('Settings'),
+                trailing: Icon(Icons.arrow_forward),
+               onTap: (){
+                  _pageController.jumpToPage(2);
+                  Navigator.pop(context);
+                    setState(() {
+                  indexBottomNavegigationBar = 2;
+            });
+                },
+              ),
+          ],
+        ),
       ),
       body: PageView(
         controller: _pageController,
         children: [
         OnePage(),
-         
         Container(color: Colors.red),
         Container(color: Colors.yellow),
       ],
